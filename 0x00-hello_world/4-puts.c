7a34cc2924d2cfde37f7afa9e0a51320e1dd98da
Theophilusarthuressel <theophilusarthuressl@gmail.com>
2023-02-17 06:34:40 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - this is the main function
- * Return: 0 if code works properly, non-zero otherwise
+ *  main - prints a given phrase
+ *  Return: 0 if exited properly, non-zero otherwise
  */
 
-int main(void) 
+int  main(void)
 {
-        puts("\"Programming is like building a multilingual puzzle");
-        return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }	
